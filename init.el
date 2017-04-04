4da5ecb011dfdc03052570a8292d93d93e1e7aea
Kyle Reeve <kreeve3@gatech.edu>
2017-04-04 07:43:52 -0400

    Saved more configs

--- init.el
+++ init.el
@@ -4,5 +4,16 @@
 (org-babel-do-load-languages
       'org-babel-load-languages
       '((emacs-lisp . t)
-        (dot . t)))
+        (dot . t)
+	(python . t)
+	(ocaml . t)
+	(C . t)))
 (setq org-confirm-babel-evaluate nil)
+(setq org-src-preserve-indentation t)
+(setq org-src-fontify-native t)
+(package-initialize)
+(require 'auto-complete)
+(global-auto-complete-mode t)
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
